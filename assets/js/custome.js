4617e21274ffa330e6eddcdc58b04bf2c18692dc
Your Name <ajisurya331@gmail.com>
2025-07-05 17:34:27 +0700

    finis sliicing dekstop view exprience, testimonial, special offer, member and footer

--- assets/js/custome.js
+++ assets/js/custome.js
@@ -6,12 +6,18 @@
             const nextBtn = wrapper.querySelector('.btnNext');
             const perPage = parseInt(wrapper.dataset.perPage) || 3;
 
+            const gap = "1rem";
+
+            if (perPage < 1){
+               const gap = "1px";
+            }
+
 
             if (!splideElement) return;
 
             const splide = new Splide(splideElement, {
                 perPage: perPage,
-                gap: "1rem",
+                gap: gap,
                 breakpoints: {
                     992: { perPage: 3 },
                     768: { perPage: 2 },
@@ -27,3 +33,13 @@
             nextBtn.addEventListener("click", () => splide.go(">"));
         });
     });
+
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#experience-slider', {
+        type: 'loop',
+        perPage: 1,
+        pagination: true,
+        arrows: false,
+        autoplay: true,
+    }).mount();
+});
